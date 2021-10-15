815d30f39632cb82d0c52c653a27b4a0b6955603
student <mpalacz00@gmail.com>
2021-10-15 11:12:42 +0200

    My work

--- MP2/src/Ćwiczenia2/Zadanie2.java
+++ MP2/src/Ćwiczenia2/Zadanie2.java
@@ -2,6 +2,20 @@ package Ćwiczenia2;
 
 public class Zadanie2 {
     public static void main(String[] args) {
+        float earthWeightPalacz;
+        float marsWeightPalacz;
+        float marsGravityPalacz;
+        double marsWeightDoublePalacz;
+        int marsWeightIntPalacz;
 
+        earthWeightPalacz = 72.85f;
+        marsGravityPalacz = 0.38f;
+        marsWeightPalacz = earthWeightPalacz * marsGravityPalacz;
+        System.out.println(marsWeightPalacz);
+
+        marsWeightDoublePalacz = marsWeightPalacz;
+        System.out.println(marsWeightDoublePalacz);
+
+        marsWeightIntPalacz = (int)marsWeightDoublePalacz;
     }
 }
